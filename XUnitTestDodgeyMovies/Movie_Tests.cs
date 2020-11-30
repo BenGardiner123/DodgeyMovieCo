7f6af2b1f486493e1901ad3b82dbad816184d655
Ben Gardiner <bennygards@gmail.com>
2020-11-30 14:19:14 +1100

    adding info for the tess

--- XUnitTestDodgeyMovies/Movie_Tests.cs
+++ XUnitTestDodgeyMovies/Movie_Tests.cs
@@ -8,13 +8,16 @@ namespace XUnitTestDodgeyMovies
     {
         Movie m1 = new DodgeyMovieCo.Movie();
 
-        //Test 1 Case 1 - Check that the http client can connect
+        //*************************************************** TEST NUMBER 1 ********************************************************
+
+        //Test 1 Case 1 - Check that the function returns an int?
         [Theory]
         [InlineData(50, 12445)]
         [InlineData(20, 862)]
         [InlineData(17, 552)]
-        public void NumActors_CheckHttp_Success(int expected, int movieNum)
+        public void NumActors_CheckIntReturned_Success(int expected, int movieNum)
         {
+            //something along the lines of isString
             Assert.Equal(expected, m1.NumActors(movieNum));
         }
 
@@ -44,44 +47,74 @@ namespace XUnitTestDodgeyMovies
         [InlineData(50, 12445)]
         [InlineData(20, 862)]
         [InlineData(17, 552)]
-        public void NumActorsTest4(int expected, int movieNum)
+        public void NumActors_InputNegNum_ThrowsEx(int expected, int movieNum)
         {
             Assert.Equal(expected, m1.NumActors(movieNum));
         }
 
-        //Test 1 Case 5 - Input is a number - but a negative one
+        //Test 1 Case 5 - Case 5 - Input is outside range of int
         [Theory]
         [InlineData(50, 12445)]
         [InlineData(20, 862)]
         [InlineData(17, 552)]
-        public void NumActorsTest5(int expected, int movieNum)
+        public void NumActors_InputOutsideRange_ThrowsEx(int expected, int movieNum)
         {
             Assert.Equal(expected, m1.NumActors(movieNum));
         }
 
+        //*************************************************** TEST NUMBER 2 ********************************************************
 
 
-
-
-
-
-
-
-
-
-
-
-
-        //these arent validated theorydata
+        //Test 2  case 1  - Check that the function returns an int?
         [Theory]
         [InlineData(50, 12445)]
         [InlineData(-29, 12445)]
 
-        public void MovieAgeTest(int expected, int movieNum)
+        public void MovieAge_CheckIntReturned_Success(int expected, int movieNum)
+        {
+            //something like IsInt()?
+            Assert.Equal(expected, m1.GetAge(movieNum));
+        }
+
+        //Test 2  case 2  - Happy Test - information entered is in the db correct output
+        [Theory]
+        [InlineData(50, 12445)]
+        [InlineData(-29, 12445)]
+
+        public void MovieAge_CorrectInput_CorreectOutput(int expected, int movieNum)
         {
             Assert.Equal(expected, m1.GetAge(movieNum));
         }
-        
-        
+
+        //Test 2  case 3 - Input is a spcial char or NaN- should throw an exception
+        [Theory]
+        [InlineData(50, 12445)]
+        [InlineData(-29, 12445)]
+
+        public void MovieAge_InputNaN_ThrowsEx(int expected, int movieNum)
+        {
+            Assert.Equal(expected, m1.GetAge(movieNum));
+        }
+
+        //Test 2  Case 4 - Input is a number - but a negative one
+        [Theory]
+        [InlineData(50, 12445)]
+        [InlineData(-29, 12445)]
+
+        public void MovieAge_InputNegNum_ThrowsEx(int expected, int movieNum)
+        {
+            Assert.Equal(expected, m1.GetAge(movieNum));
+        }
+
+        //Test 2  Case 5 - Input is outside range of int
+        [Theory]
+        [InlineData(50, 12445)]
+        [InlineData(-29, 12445)]
+
+        public void MovieAge_InputOutsideRange_ThrowsEx(int expected, int movieNum)
+        {
+            Assert.Equal(expected, m1.GetAge(movieNum));
+        }
+
     }
 }
